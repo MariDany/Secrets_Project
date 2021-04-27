9f00132234ae540b11c4610381a71ae1cede99e6
Maria Atencio <maridany0597@gmail.com>
2021-04-27 18:40:34 -0400

    Update Authentication to Level 2

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption')
 
 const app = express();
 
@@ -23,6 +24,10 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 })
+
+const secret = "Thisisourlittlesecret.";
+
+userSchema.plugin(encrypt,{secret: secret,  encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
