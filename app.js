0c86e59d579a6c498f518199872ea98d742a0a91
Maria Atencio <maridany0597@gmail.com>
2021-04-27 19:35:32 -0400

    Level 3 Authentication - Hash

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption')
+const md5 = require('md5');
 
 const app = express();
 
@@ -27,9 +27,6 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-const secret = process.env.SECRET;
-
-userSchema.plugin(encrypt,{secret: secret,  encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -50,7 +47,7 @@ app.post('/login', function(req,res){
             console.log(err);
         } else {
             if(foundUser){
-                if(foundUser.password === password){
+                if(foundUser.password === (md5(password))){
                     res.render('Secrets');
                 }
             }
@@ -64,7 +61,7 @@ app.get('/register', function(req,res){
 app.post("/register", function(req,res){
   const newUser = new User({
      email: req.body.username,
-     password: req.body.password
+     password: md5(req.body.password)
   });
   newUser.save(function(err){
       if(err){
